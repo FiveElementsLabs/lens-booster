0cdfd8abaeeaaef7fcab55297678c067fe2b7f21
Christian Kobril <christiankobril@gmail.com>
2022-04-22 20:38:23 +0200

    feat(home): inital layout setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Router from './Router'
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <Router />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/lib/ConfigVars.js
@@ -0,0 +1,18 @@
+/*
+ *  These are publicly available variables used in the app.
+ */
+
+export const APP_NAME = 'LensWindmill';
+export const APP_DESC = '';
+
+export const LENS_API_URL = 'https://api-mumbai.lens.dev/';
+export const MUMBAI_RPC_URL = 'https://rpc-mumbai.matic.today';
+export const MUMBAI_SCAN = 'https://mumbai.polygonscan.com/';
+
+export const LENS_HUB_CONTRACT = '0xd7B3481De00995046C7850bCe9a5196B7605c367';
+
+// Metadata Standards can change.
+// Check for updates here: https://docs.lens.dev/docs/metadata-standards
+export const LENS_PUBLICATION_METADATA_VERSION = '1.0.0';
+
+export const APP_ID = 'testing-windmill';
