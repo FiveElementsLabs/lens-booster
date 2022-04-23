d1965e22244348991895ce17dd1c84c5ca3efa81
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2022-04-23 16:34:21 +0200

    Update Settings.js

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
   Box,
   Button,
@@ -7,64 +7,53 @@ import {
   Container,
   FormControl,
   FormLabel,
+  Grid,
+  GridItem,
   Input,
   Textarea,
   Text,
   Select,
   useColorModeValue,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-import { useProfile } from "../hooks/useProfile";
-import { createProfile } from "../api/profile/create-profile";
-import { updateProfile } from "../api/profile/update-profile";
-import { useSharedState } from "../context/store";
-import { createPost } from "../api/publications/post";
-import { getPublications } from "../api/publications/get-publications";
+import { useProfile } from '../hooks/useProfile';
+import { createProfile } from '../api/profile/create-profile';
+import { updateProfile } from '../api/profile/update-profile';
+import { useSharedState } from '../context/store';
+import { createPost } from '../api/publications/post';
+import { CreateIndex } from '../components/superfluid/createIndex';
+import { UpdateSubscription } from '../components/superfluid/updateSubscription';
+import { DistributeFunds } from '../components/superfluid/distributeFunds';
 
 export default function Settings() {
   const [{ account, provider }] = useSharedState();
   const { profiles, currentProfile } = useProfile();
   const [selectedProfile, setSelectedProfile] = useState({});
   const [postMetaData, setPostMetaData] = useState({});
-  const [posts, setPosts] = useState([]);
 
-  const [message, setMessage] = useState("");
-  const [handle, setHandle] = useState("");
+  const [message, setMessage] = useState('');
+  const [handle, setHandle] = useState('');
   const [profileMetaData, setProfileMetaData] = useState({});
   const toast = useToast();
 
-  console.log(currentProfile)
   useEffect(() => {
     if (profiles) {
       setSelectedProfile(currentProfile);
     }
   }, [profiles, currentProfile]);
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const publications = await getPublications();
-        console.log(publications)
-
-      } catch (err) {
-        console.error("LOADING ERROR in ProfilePage: ", err?.message);
-      }
-    };
-    loadData();
-  }, [posts]);
-
   const onCreateProfile = async (e) => {
     e.preventDefault();
-    console.log("onCreateProfile: ", account + " " + handle);
+    console.log('onCreateProfile: ', account + ' ' + handle);
     try {
       const signer = await provider.getSigner();
       const res = await createProfile(account, handle, signer);
       setMessage(res);
       toast({
-        title: "New profile created",
-        status: "success",
-        position: "bottom-right",
-        variant: "subtle",
+        title: 'New profile created',
+        status: 'success',
+        position: 'bottom-right',
+        variant: 'subtle',
       });
     } catch (err) {
       console.error(err?.message);
@@ -91,10 +80,10 @@ export default function Settings() {
       // See api/profile/update-profile for full metadata types.
       await updateProfile(account, profileMetaData);
       toast({
-        title: "Profile updated",
-        status: "success",
-        position: "bottom-right",
-        variant: "subtle",
+        title: 'Profile updated',
+        status: 'success',
+        position: 'bottom-right',
+        variant: 'subtle',
       });
     } catch (err) {
       console.error(err?.message);
@@ -117,206 +106,165 @@ export default function Settings() {
 
   return (
     <>
-      <Box
-        mx="auto"
-        maxW="container.md"
-        rounded="xl"
-        mt={5}
-        p={4}
-        bg={useColorModeValue("#ECF1FE", "#2c5410")}
-      >
-        <Text>Create new Profile</Text>
-        <form onSubmit={onCreateProfile}>
-          <FormControl mt={5} isRequired>
-            <FormLabel htmlFor="handle">Handle</FormLabel>
-            <Input
-              id="handle"
-              type="text"
-              onChange={(e) => setHandle(e.target.value)}
-              bg={useColorModeValue("white", "dark_background")}
-              color={useColorModeValue("black", "black")}
-            />
-          </FormControl>
-          <Button
-            bg={useColorModeValue("yellow_accent", "#3a3a3a")}
-            color={"white"}
-            mt={5}
-            type="submit"
-          >
-            Create Profile
-          </Button>
-        </form>
-      </Box>
+      <Grid h="200px" templateRows="repeat(2, 1fr)" templateColumns="repeat(5, 1fr)" mt={4}>
+        <GridItem colSpan={{ base: '5', md: '2' }} m={2}>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <CreateIndex></CreateIndex>
+          </Box>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <UpdateSubscription></UpdateSubscription>
+          </Box>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <DistributeFunds></DistributeFunds>
+          </Box>
 
-      <Box
-        mx="auto"
-        maxW="container.md"
-        rounded="xl"
-        mt={5}
-        p={4}
-        bg={useColorModeValue("#ECF1FE", "#2c5410")}
-      >
-        <Text>Update one of my profiles</Text>
-        {/* Possible fields: profileId, name, bio, location, website, twitterUrl, coverPicture */}
-        <form onSubmit={onUpdateProfile}>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="name">Handle</FormLabel>
-            <Input
-              id="name"
-              value={selectedProfile ? selectedProfile.handle : ""}
-              type="text"
-              onChange={(e) => updateProfileMetaData(e, "name")}
-              disabled={true}
-              bg={useColorModeValue("white", "dark_background")}
-              color={useColorModeValue("black", "black")}
-            />
-          </FormControl>
-          <FormControl mt={5} isRequired>
-            <FormLabel htmlFor="profileId">Profile ID</FormLabel>
-            <Select
-              placeholder="Select profile"
-              id="profileId"
-              bg={useColorModeValue("white", "dark_background")}
-              defaultValue={selectedProfile ? selectedProfile.id : ""}
-              onChange={(e) => {
-                let profile = profiles.find((p) => p.id === e.target.value);
-                setSelectedProfile(profile);
-                setProfileMetaData({
-                  name: profile.name,
-                  bio: profile.bio,
-                  profileId: e.target.value,
-                });
-              }}
-            >
-              {profiles?.map((profile, index) => {
-                return (
-                  <option key={index} value={profile.id}>
-                    {profile.id}
-                  </option>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input
-              bg={useColorModeValue("white", "dark_background")}
-              id="name"
-              defaultValue={selectedProfile ? selectedProfile.name : ""}
-              type="text"
-              onChange={(e) => updateProfileMetaData(e, "name")}
-              color={useColorModeValue("black", "black")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="bio">Bio</FormLabel>
-            <Textarea
-              id="bio"
-              onChange={(e) => updateProfileMetaData(e, "bio")}
-              defaultValue={selectedProfile ? selectedProfile.bio : ""}
-              bg={useColorModeValue("white", "dark_background")}
-              color={useColorModeValue("black", "black")}
-            />
-          </FormControl>
-          <Button
-            bg={useColorModeValue("yellow_accent", "#3a3a3a")}
-            color={"white"}
-            mt={5}
-            type="submit"
-          >
-            Update this profile
-          </Button>
-        </form>
-      </Box>
-
-      <Box
-        mx="auto"
-        mt={5}
-        maxW="container.md"
-        border="1px solid gray"
-        rounded="xl"
-        p={4}
-      >
-        <Text>Create new Post</Text>
-        {/* Possible fields: profileId, name, description, external_url, image, imageMimeType, content */}
-        <form onSubmit={onCreatePost}>
-          <FormControl mt={5} isRequired>
-            <FormLabel htmlFor="profileId">Profile ID</FormLabel>
-            <Input
-              id="profileId"
-              type="text"
-              onChange={(e) => updatePostMetaData(e, "profileId")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input
-              id="name"
-              type="text"
-              onChange={(e) => updatePostMetaData(e, "name")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="description">Description</FormLabel>
-            <Input
-              id="description"
-              type="text"
-              onChange={(e) => updatePostMetaData(e, "description")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="external_url">External URL</FormLabel>
-            <Input
-              id="external_url"
-              type="text"
-              onChange={(e) => updatePostMetaData(e, "external_url")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="image">Image URL</FormLabel>
-            <Input
-              id="image"
-              type="text"
-              onChange={(e) => updatePostMetaData(e, "image")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="imageMimeType">Image MimeType</FormLabel>
-            <Input
-              id="imageMimeType"
-              type="text"
-              placeholder="image/jpeg"
-              onChange={(e) => updatePostMetaData(e, "imageMimeType")}
-            />
-          </FormControl>
-          <FormControl mt={5}>
-            <FormLabel htmlFor="content">Content</FormLabel>
-            <Textarea
-              id="content"
-              onChange={(e) => updatePostMetaData(e, "content")}
-            />
-          </FormControl>
-          <Button mt={5} type="submit" colorScheme="green">
-            Create Post
-          </Button>
-        </form>
-      </Box>
-
-      <Box
-        mx="auto"
-        maxW="container.md"
-        rounded="xl"
-        mt={5}
-        p={4}
-        bg={useColorModeValue("#ECF1FE", "#2c5410")}
-      >
-        <Text>My posts</Text>
-      </Box>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <Text>Create new Profile</Text>
+            <form onSubmit={onCreateProfile}>
+              <FormControl mt={5} isRequired>
+                <FormLabel htmlFor="handle">Handle</FormLabel>
+                <Input
+                  id="handle"
+                  type="text"
+                  onChange={(e) => setHandle(e.target.value)}
+                  color={useColorModeValue('black', 'black')}
+                />
+              </FormControl>
+              <Button mt={5} type="submit" colorScheme="teal" variant="outline">
+                Create Profile
+              </Button>
+            </form>
+          </Box>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <Text>Update one of my profiles</Text>
+            {/* Possible fields: profileId, name, bio, location, website, twitterUrl, coverPicture */}
+            <form onSubmit={onUpdateProfile}>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="name">Handle</FormLabel>
+                <Input
+                  id="name"
+                  value={selectedProfile ? selectedProfile.handle : ''}
+                  type="text"
+                  onChange={(e) => updateProfileMetaData(e, 'name')}
+                  disabled={true}
+                  bg={useColorModeValue('white', 'dark_background')}
+                  color={useColorModeValue('black', 'black')}
+                />
+              </FormControl>
+              <FormControl mt={5} isRequired>
+                <FormLabel htmlFor="profileId">Profile ID</FormLabel>
+                <Select
+                  placeholder="Select profile"
+                  id="profileId"
+                  defaultValue={selectedProfile ? selectedProfile.id : ''}
+                  onChange={(e) => {
+                    let profile = profiles.find((p) => p.id === e.target.value);
+                    setSelectedProfile(profile);
+                    setProfileMetaData({
+                      name: profile.name,
+                      bio: profile.bio,
+                      profileId: e.target.value,
+                    });
+                  }}
+                >
+                  {profiles?.map((profile, index) => {
+                    return (
+                      <option key={index} value={profile.id}>
+                        {profile.id}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input
+                  id="name"
+                  defaultValue={selectedProfile ? selectedProfile.name : ''}
+                  type="text"
+                  onChange={(e) => updateProfileMetaData(e, 'name')}
+                  color={useColorModeValue('white', 'black')}
+                />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="bio">Bio</FormLabel>
+                <Textarea
+                  id="bio"
+                  onChange={(e) => updateProfileMetaData(e, 'bio')}
+                  defaultValue={selectedProfile ? selectedProfile.bio : ''}
+                  color={useColorModeValue('white', 'black')}
+                />
+              </FormControl>
+              <Button mt={5} type="submit" colorScheme="teal" variant="outline">
+                Update this profile
+              </Button>
+            </form>
+          </Box>
+        </GridItem>
+        <GridItem colSpan={{ base: '5', md: '3' }} m={2}>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <Text>Create new Post</Text>
+            {/* Possible fields: profileId, name, description, external_url, image, imageMimeType, content */}
+            <form onSubmit={onCreatePost}>
+              <FormControl mt={5} isRequired>
+                <FormLabel htmlFor="profileId">Profile ID</FormLabel>
+                <Input
+                  id="profileId"
+                  type="text"
+                  onChange={(e) => updatePostMetaData(e, 'profileId')}
+                />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input id="name" type="text" onChange={(e) => updatePostMetaData(e, 'name')} />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="description">Description</FormLabel>
+                <Input
+                  id="description"
+                  type="text"
+                  onChange={(e) => updatePostMetaData(e, 'description')}
+                />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="external_url">External URL</FormLabel>
+                <Input
+                  id="external_url"
+                  type="text"
+                  onChange={(e) => updatePostMetaData(e, 'external_url')}
+                />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="image">Image URL</FormLabel>
+                <Input id="image" type="text" onChange={(e) => updatePostMetaData(e, 'image')} />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="imageMimeType">Image MimeType</FormLabel>
+                <Input
+                  id="imageMimeType"
+                  type="text"
+                  placeholder="image/jpeg"
+                  onChange={(e) => updatePostMetaData(e, 'imageMimeType')}
+                />
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="content">Content</FormLabel>
+                <Textarea id="content" onChange={(e) => updatePostMetaData(e, 'content')} />
+              </FormControl>
+              <Button mt={5} type="submit" colorScheme="teal" variant="outline">
+                Create Post
+              </Button>
+            </form>
+          </Box>
+          <Box mx="auto" mt={5} maxW="container.md" border="1px solid gray" rounded="xl" p={4}>
+            <Text>My posts</Text>
+          </Box>
+        </GridItem>
+        <GridItem colSpan={{ base: '5', md: '3' }} m={2}></GridItem>
+      </Grid>
 
       <Container maxW="container.md" mt={10}>
-        <Code maxW="container.md">
-          {message ? JSON.stringify(message) : ""}
-        </Code>
+        <Code maxW="container.md">{message ? JSON.stringify(message) : ''}</Code>
       </Container>
     </>
   );
