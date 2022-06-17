41cf65e9d4c6928f4716affb0dd1b553b7a2ad4e
alby.giaco <alby.giaco@live.it>
2022-06-17 10:36:25 +0200

    fix(conflict)

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -34,13 +34,6 @@ const NAV_ITEMS = [
   {
     label: 'INFLENSER',
     href: '/inflenser',
-<<<<<<< HEAD
-  },
-  {
-    label: 'ADVERTISER',
-    href: '/settings',
-=======
->>>>>>> 763a9e041228ddf03a13a7029e21f4b99db074d8
   },
 ];
 
