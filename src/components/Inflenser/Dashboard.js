c7d965cb5026f51e58ad870309a2c5dea1267903
alby.giaco <alby.giaco@live.it>
2022-06-21 10:46:04 +0200

    feat(dashboard): do not display dashboard and campaign info if not applyed to campaign

--- src/components/Inflenser/Dashboard.js
+++ src/components/Inflenser/Dashboard.js
@@ -61,46 +61,48 @@ export default function Dashboard() {
   }, [provider]);
   return (
     <>
-      <Box mt={8} p={5} borderRadius="20px" boxShadow="lg" bg="white" fontFamily="'Prompt', sans-serif" width="auto">
-        <Flex alignItems="center" display={{ base: 'block', md: 'flex' }}>
-          <Text color="#FF6827" fontSize={26} textAlign="left" mb={{ base: '10px', md: '0' }}>
-            Total earned
-            <Text
-              color="#FF6827"
-              fontSize={30}
-              textAlign="left"
-              verticalAlign="center"
-              fontWeight={600}
-              lineHeight={{ base: 1, md: 1.5 }}
-            >
-              $ {totalEarn / 1e6}
+      {totalEarn != 0 && (
+        <Box mt={8} p={5} borderRadius="20px" boxShadow="lg" bg="white" fontFamily="'Prompt', sans-serif" width="auto">
+          <Flex alignItems="center" display={{ base: 'block', md: 'flex' }}>
+            <Text color="#FF6827" fontSize={26} textAlign="left" mb={{ base: '10px', md: '0' }}>
+              Total earned
+              <Text
+                color="#FF6827"
+                fontSize={30}
+                textAlign="left"
+                verticalAlign="center"
+                fontWeight={600}
+                lineHeight={{ base: 1, md: 1.5 }}
+              >
+                $ {totalEarn / 1e6}
+              </Text>
             </Text>
-          </Text>
 
-          <Spacer />
-          <Text color="#1A4587" fontSize={24} textAlign="left" mb={{ base: '10px', md: '0' }}>
-            Total mirrors
-            <Text color="#00203F" fontSize={24} verticalAlign="center" textAlign="left" fontWeight={600}>
-              {totalMirror}
+            <Spacer />
+            <Text color="#1A4587" fontSize={24} textAlign="left" mb={{ base: '10px', md: '0' }}>
+              Total mirrors
+              <Text color="#00203F" fontSize={24} verticalAlign="center" textAlign="left" fontWeight={600}>
+                {totalMirror}
+              </Text>
             </Text>
-          </Text>
-          <Spacer />
-          <Text color="#1A4587" fontSize={24} textAlign="left" mb={{ base: '10px', md: '0' }}>
-            Total clicks
-            <Text color="#00203F" fontSize={24} textAlign="left" fontWeight={600}>
-              {totalClicks}
+            <Spacer />
+            <Text color="#1A4587" fontSize={24} textAlign="left" mb={{ base: '10px', md: '0' }}>
+              Total clicks
+              <Text color="#00203F" fontSize={24} textAlign="left" fontWeight={600}>
+                {totalClicks}
+              </Text>
             </Text>
-          </Text>
-          <Spacer />
-          <Text color="#1A4587" fontSize={24} textAlign="left">
-            Total events
-            <Text color="#00203F" fontSize={24} textAlign="left" fontWeight={600}>
-              {totalEvents}
+            <Spacer />
+            <Text color="#1A4587" fontSize={24} textAlign="left">
+              Total events
+              <Text color="#00203F" fontSize={24} textAlign="left" fontWeight={600}>
+                {totalEvents}
+              </Text>
             </Text>
-          </Text>
-          <Spacer />
-        </Flex>
-      </Box>
+            <Spacer />
+          </Flex>
+        </Box>
+      )}
 
       {isLargerThan640 ? <AdvertisersStats /> : <AdvertisersStatsMobile />}
     </>
