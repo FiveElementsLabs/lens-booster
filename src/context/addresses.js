537b828657ec05834a4c2952fa5abd9e4aa34c1f
alby.giaco <alby.giaco@live.it>
2022-06-22 15:03:57 +0200

    chore(address): change address

--- src/context/addresses.js
+++ src/context/addresses.js
@@ -1,4 +1,4 @@
 export const addresses = {
-  CampaignManager: '0xBbb38450ECD85DF3f72Dc0a1CEc316Faaca56910',
+  CampaignManager: '0xFc336844Ac4D89BdE83640D99545524d80F0B2c7',
   LensHub: '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d',
 };
