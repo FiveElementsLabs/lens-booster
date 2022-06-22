124cc4506039dbc48c78666e3faa62ffcc0f67f9
alby.giaco <alby.giaco@live.it>
2022-06-22 14:32:44 +0200

    feat(do not show campaign

--- src/components/Inflenser/Campaigns.js
+++ src/components/Inflenser/Campaigns.js
@@ -1,21 +1,22 @@
 import { Box, Text, Flex, Heading } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useCampaignManager } from '../../hooks/useCampaignManager';
+import { useCampaign } from '../../hooks/useCampaign';
 import { useSharedState } from '../../context/store';
 
 import PostCard from './PostCard';
 
 export default function Dashboard() {
-  const { getCampaignsPublicationID } = useCampaignManager();
+  const { getCampaignsPublicationID, getCampaigns } = useCampaignManager();
   const [{ provider }, dispatch] = useSharedState();
   const [publicationIds, setPublicationIds] = useState();
 
   useEffect(() => {
-    const getPubIds = async () => {
+    const getPubIdsData = async () => {
       const pubs = await getCampaignsPublicationID();
       setPublicationIds(pubs);
     };
-    getPubIds();
+    getPubIdsData();
   }, [provider]);
   return (
     <>
